82fb12fb6a3b810ee24f0def8de4774c37227626
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-09-24 17:48:48 +0200

    Validate query parameters and allow ids

--- src/controllers/service-stations.controller.ts
+++ src/controllers/service-stations.controller.ts
@@ -1,13 +1,50 @@
 import { Request, Response } from "express";
 import { FuelStation } from "../models/db/fuel-station";
+import { query, validationResult } from "express-validator";
+import defaults from '../config/defaults.json'
+
+export const serviceStationsValidations = [
+  query('limit').isInt().optional().withMessage('Limit parameter must be an int value'),
+  query('offset').isInt().optional().withMessage('Offset parameter must be an int value'),
+  query('id')
+    .custom(value => {
+      if (typeof value === 'string') {
+        return true;
+      }
+      if (Array.isArray(value) && value.every(item => typeof item === 'string')) {
+        return true;
+      }
+      throw new Error('Id must be a string or an array of strings');
+    })
+];
 
 export const serviceStationsController = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
   try {
-    const results = await FuelStation.findAndCountAll({
-      limit: 30
+    const limit = req.query.limit ? parseInt(req.query.limit as string) : defaults.query.limit
+    const offset = req.query.offset ? parseInt(req.query.offset as string) : defaults.query.offset
+
+    let where = {}
+    if (req.query.id) {
+      where = {
+        ...where,
+        IDEESS: req.query.id
+      }
+    }
+
+    const results = await FuelStation.findAll({
+      limit: !req.query.id ? limit : undefined,
+      offset: !req.query.id ? offset : undefined,
+      where
     })
     res.json(results)
   } catch (error) {
+    console.log(error)
     res.sendStatus(500)
   }
 }
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { serviceStationsController } from '../controllers/service-stations.controller';
+import { serviceStationsController, serviceStationsValidations } from '../controllers/service-stations.controller';
 
 const router: Router = Router();
 
-router.get('/service-stations', serviceStationsController)
+router.get('/service-stations', serviceStationsValidations, serviceStationsController)
 
 router.get('*', (req, res) => {
   res.status(404).send("Endpoint not found")
