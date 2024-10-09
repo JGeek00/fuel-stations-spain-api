514f8f2da48a29f0bc228a1b0e4544b1a89146aa
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-10-09 15:43:55 +0200

    Added sentry

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,27 @@ import express from 'express';
 import * as dotevnv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import database from '@/services/database';
 import Router from '@/routes/router'
 
 dotevnv.config()
 
 const app = express();
+
+if (process.env.SENTRY_DSN && process.env.PRODUCTION == "true") {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      nodeProfilingIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.expressErrorHandler);
+}
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
